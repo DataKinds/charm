4fbc5d343bd4c01263e11548617f6e613fd28cba
Aearnus <tslimkemann42@gmail.com>
2018-09-12 15:49:02 -0700

    lexer checkin

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,6 +1,11 @@
 #include "Lexer.h"
 
 #include <iterator>
+#include <string>
+
+Lexer::Lexer(std::string line) {
+    this.rest = line;
+}
 
 std::optional<std::string> Lexer::consumeOpenBracket() {
     if (this.rest.at(0) == '[') {
@@ -57,11 +62,125 @@ std::optional<std::string> Lexer::consumeString() {
 }
 std::optional<std::string> Lexer::consumeNumber() {
     std::string out = "";
-    // TODO
-}
-std::optional<std::string> Lexer::consumeIdent() {
-    // TODO
+    // First, trim out any possible positive/negative signs
+    if (this.rest.at(0) == '-') {
+        out = "-" + out;
+        this.rest = this.rest.substr(1);
+    }
+    else if (this.rest.at(0) == '+') {
+        this.rest = this.rest.substr(1);
+    }
+    // Then make sure we have an actual number
+    if (std::isdigit(this.rest.at(0))) {
+        while (std::isdigit(this.rest.at(0)) {
+            // Pop that number into out
+            out = out + this.rest.substr(0, 1);
+            this.rest = this.rest.substr(1);
+            // Check edge cases
+            if (this.rest.length() == 0) {
+                break;
+            }
+            if (this.rest.at(0) == '.') {
+                out = "." + out;
+                this.rest = this.res.substr(1);
+            }
+        }
+        return
+    } else {
+        return std::nullopt;
+    }
 }
 std::optional<std::string> Lexer::consumeWhitespace() {
-    // TODO
+    std::string out = "";
+    if (std::isspace(this.rest.at(0))) {
+        while (std::isspace(this.rest.at(0))) {
+            out = out + this.rest.at(0);
+            this.rest = this.rest.substr(1);
+            if (this.rest.length() == 0) {
+                return out;
+            }
+        }
+        return out;
+    } else {
+        return std::nullopt;
+    }
 }
+std::optional<std::string> Lexer::consumeIdent() {
+    std::string out = "";
+    // This comes after consuming whitespace and consuming
+    // a number, so it matches very greedily
+    if (!std::isspace(this.rest.at(0))) {
+        while (!std::isspace(this.rest.at(0))) {
+            out = out + this.rest.at(0);
+            this.rest = this.rest.substr(1);
+            if (this.rest.length() == 0) {
+                return out;
+            }
+        }
+        return out;
+    }
+    return std::nullopt;
+}
+
+Lexeme Lexer::consumeLexeme() {
+    std::string backtrack = this.rest;
+    // All this code is the equivalent of chaining with the `<<`
+    // operator in Haskell
+    Lexeme lexeme;
+    std::optional<std::string> = lexeme_str;
+
+    if (Lexer::consumeOpenBracket()) {
+        lexeme.lexeme = (struct OpenBracket){};
+        goto continue;
+    }
+    if (Lexer::consumeCloseBracket()) {
+        lexeme.lexeme = (struct CloseBracket){};
+        goto continue;
+    }
+    if (Lexer::consumeLineBreak()) {
+        lexeme.lexeme = (struct LineBreak){};
+        goto continue;
+    }
+    if (Lexer::consumeSingleColon()) {
+        lexeme.lexeme = (struct SingleColon){};
+        goto continue;
+    }
+    if (Lexer::consumeArrow()) {
+        lexeme.lexeme = (struct Arrow){};
+        goto continue;
+    }
+    // the above methods do not need backtracking
+    // the below methods do
+    if (lexeme_str = Lexer::consumeString()) {
+        lexeme.lexeme = (struct String){ .string = lexeme_str.value() };
+        goto continue;
+    }
+    this.rest = backtrack;
+    if (lexeme_str = Lexer::consumeNumber()) {
+        lexeme.lexeme = (struct Number){ .number = lexeme_str.value() };
+        goto continue;
+    }
+    this.rest = backtrack;
+    if (lexeme_str = Lexer::consumeIdent()) {
+        lexeme.lexeme = (struct Ident){ .ident = lexeme_str.value() };
+        goto continue;
+    }
+    // Nothing matched, so we error
+    parsetime_die("AAAAA");
+    continue:
+    return lexeme;
+}
+
+std::vector<Lexeme> Lexer::consumeAllLexemes() {
+    std::vector<Lexeme> out;
+    while (this.rest.length() > 0) {
+        while (Lexer::consumeWhitespace()) {}
+        out.push_back(Lexer::consumeLexeme());
+    }
+}
+
+// f : R^2 -> R^3
+// f ([x; y]) = [x; y; 0]
+
+// g : R^2 -> R^2
+// g rotates a vector 45 degrees clockwise
--- Lexer.h
+++ Lexer.h
@@ -22,15 +22,16 @@ struct Lexeme {
 class Lexer {
 public:
     // all of these functions will fail if fed an empty string
+    // it is NOT these functions' job to backtrack on failure
     std::optional<std::string> consumeOpenBracket();
     std::optional<std::string> consumeCloseBracket();
     std::optional<std::string> consumeLineBreak();
     std::optional<std::string> consumeSingleColon();
     std::optional<std::string> consumeArrow();
+    std::optional<std::string> consumeWhitespace();
     std::optional<std::string> consumeString();
     std::optional<std::string> consumeNumber();
     std::optional<std::string> consumeIdent();
-    std::optional<std::string> consumeWhitespace();
 
     Lexeme consumeLexeme();
     std::vector<Lexeme> consumeAllLexemes();
